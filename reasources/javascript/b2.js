392235471c99d038c2b2b1f1b914fe3e9d32058a
Ryan Devine <rj.devine@outlook.com>
2022-01-27 12:11:24 +0000

    added the start of the javascript

--- reasources/javascript/b2.js
+++ reasources/javascript/b2.js
@@ -1,6 +1,15 @@
-// const artImg = document.getElementById('artImg')
-// artImg.addEventListener('click,',function () {
-//     console.log('lol')
-//     window.location.href = "lights.html?roomID=" ;
-// })
+const artImg = document.getElementById('artImg')
+artImg.addEventListener('click,',function () {
+    console.log('lol')
+    
+        window.open("/S6C-B2/CreativeM/art.html")
+})
 
+function websiteLink() {
+    window.open("/S6C-B2/CreativeM/art.html")
+}
+
+// const artImg = document.getElementById('artImg')
+// artImg.addEventListener('onclick,',function websiteLink() {
+//         window.open("/S6C-B2/CreativeM/art.html")
+// })
