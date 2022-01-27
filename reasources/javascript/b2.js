786be342668462bc18c99eb080ee10b0afe5b94f
Ryan Devine <rj.devine@outlook.com>
2022-01-27 11:22:28 +0000

    added arts

---
+++ reasources/javascript/b2.js
@@ -0,0 +1,6 @@
+// const artImg = document.getElementById('artImg')
+// artImg.addEventListener('click,',function () {
+//     console.log('lol')
+//     window.location.href = "lights.html?roomID=" ;
+// })
+
