994a0c8ba2e7f80df5955a31ea04cf20bbad74b1
Ryan Devine <rj.devine@outlook.com>
2022-03-08 14:04:13 +0000

    m

---
+++ public/reasources/javascript/b2.js
@@ -0,0 +1,15 @@
+const artImg = document.getElementById('artImg')
+artImg.addEventListener('click,',function () {
+    console.log('lol')
+    
+        window.open("/S6C-B2/CreativeM/art.html")
+})
+
+function artClick() {
+    window.open("/S6C-B2/CreativeM/art.html")
+}
+
+// const artImg = document.getElementById('artImg')
+// artImg.addEventListener('onclick,',function websiteLink() {
+//         window.open("/S6C-B2/CreativeM/art.html")
+// })
