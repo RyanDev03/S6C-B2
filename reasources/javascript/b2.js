2a4257199b05aa9351361d07bf2acd2e6a0da422
Ryan Devine <rj.devine@outlook.com>
2022-05-05 14:08:25 +0100

    tooka away firebase

---
+++ reasources/javascript/b2.js
@@ -0,0 +1,12 @@
+// const artImg = document.getElementById('Media')
+// artImg.addEventListener('click,',function () {
+//     console.log('lol')
+    
+//         window.open("CreativeM/art.html")
+// })
+
+// function mediaClick() {
+//     window.open("CreativeM/art.html")
+// }
+
+// window.location.replace("/Users/ryandevine/github/finlaNoFire/CreativeM/art.html")
