5e2b4161708c7254a51f1232d68efbbf036a5f98
Ryan Devine <rj.devine@outlook.com>
2022-05-05 13:27:59 +0100

    added some stuff

---
+++ public/firebase.js
@@ -0,0 +1,15 @@
+
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwyx85MkuFCh7Ll6E0yNhnrtRD--cwlAE",
+  authDomain: "final-b2.firebaseapp.com",
+  databaseURL: "https://final-b2-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "final-b2",
+  storageBucket: "final-b2.appspot.com",
+  messagingSenderId: "421893644224",
+  appId: "1:421893644224:web:94c9ebb31534fe1c02f225",
+  measurementId: "G-3DG9YK92S5"
+};
